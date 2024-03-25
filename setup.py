89c55e148d0cd6e23d748332bc66dde9a23a432b
Muhammad Salman Kamran <salman.kamran.0079@gmail.com>
2024-03-25 14:12:45 +0500

    Template and setup file created with requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Cat Vs Dog Classifier',
+    version='0.0.1',
+    description='This is simple Dog Vs Cat Classifier',
+    author='MuhammadSalman-Kamran',
+    author_email='salman.kamran.0079@gmail.com',
+    packages= find_packages()
+)
---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
+project_name = 'CatvsDog-Deployment'
+files_list = [
+    '.github/workflows/.gitkeep',
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
+    'config/config.yaml',
+    'research/practice.ipynb',
+    'requirements.txt',
+    'setup.py',
+    'params.yaml'
+]
+
+for filepath in files_list:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory {filedir} for file: {filename}')
+
+    if (not os.path.exists(filepath) or os.path.getsize(filepath) != 0):
+        with open(filepath, 'w') as f:
+            pass
+
+    else:
+        logging.info(f'{filename} is already exist')
